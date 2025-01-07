4b55932fdc5a3c5cf3a5b51044a04d32c326c9a3
Paavo Nätti <paavo.natti@gmail.com>
2025-01-07 19:09:34 +0200

    Project v0.1

---
+++ todo_application/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello world")
+}
+
+
+func main() {
+    port := 8000
+    portstr, ok := os.LookupEnv("PORT")
+    if ok {
+        envport, err := strconv.Atoi(portstr)
+        if err != nil {
+            port = envport
+        }
+    }
+    addr := fmt.Sprintf(":%d", port)
+    fmt.Printf("Server started in port %d\n", port)
+
+    http.HandleFunc("/", indexHandler)
+    log.Fatal(http.ListenAndServe(addr, nil))
+}
