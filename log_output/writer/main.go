13eadc1baec8f148ce58f4693b159b583a44608e
Paavo Nätti <paavo.natti@gmail.com>
2025-01-11 13:27:06 +0200

    Exercise 1.07

--- log_output/writer/main.go
+++ log_output/writer/main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
 )
 
+var lastTime string
+var randomString string
+
+type JsonResponse struct {
+    Time string
+    String string
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+    _ = r
+    data := JsonResponse{ Time: lastTime, String: randomString }
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(data)
+}
+
 func main() {
-    str := uuid.New().String()
+    randomString = uuid.New().String()
+
+    port := 8000
+    portstr, ok := os.LookupEnv("PORT")
+    if ok {
+        envport, err := strconv.Atoi(portstr)
+        if err == nil {
+            port = envport
+        }
+    }
+    addr := fmt.Sprintf(":%d", port)
+    fmt.Printf("Server started in port %d\n", port)
+    http.HandleFunc("/status", statusHandler)
+    go http.ListenAndServe(addr, nil)
+
     for {
-        t := time.Now().UTC().String()
-        fmt.Printf("%s: %s\n", t, str)
+        lastTime = time.Now().UTC().String()
+        fmt.Printf("%s: %s\n", lastTime, randomString)
         time.Sleep(5 * time.Second)
     }
 }
