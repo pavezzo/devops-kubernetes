a2af257d79cf0beca6179f1ab4f793eed61dfc59
Paavo Nätti <paavo.natti@gmail.com>
2025-01-27 15:59:14 +0200

    Project v1.2

--- todo_application/todo-backend/main.go
+++ todo_application/todo-backend/main.go
@@ -1,46 +1,121 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/jackc/pgx/v5"
 )
 
 type TodoPostReq struct {
     Todo string
 }
 
-var todos []string
+var conn *pgx.Conn
+
+func getConnection() (*pgx.Conn, error) {
+    if conn == nil || conn.IsClosed() {
+        dbport := os.Getenv("DB_PORT")
+        dbpass := os.Getenv("DB_PASSWORD")
+        dbhost := os.Getenv("DB_HOST")
+        dbuser := os.Getenv("DB_USER")
+        dbname := os.Getenv("DB_NAME")
+        connstr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s", dbuser, dbpass, dbhost, dbport, dbname)
+
+        var err error
+        conn, err = pgx.Connect(context.Background(), connstr)
+        if err != nil {
+            return nil, err
+        }
+    }
+
+    return conn, nil
+}
+
+
+func getTodos() ([]string, error) {
+    var todos []string
+
+    dbconn, err := getConnection()
+    if err != nil {
+        return todos, err
+    }
+
+    rows, err := dbconn.Query(context.Background(), "SELECT todo FROM todos")
+    if err != nil {
+        return todos, err
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+        var todo string
+        err := rows.Scan(&todo)
+        if err != nil {
+            return todos, err
+        }
+
+        todos = append(todos, todo)
+    }
+
+    return todos, nil
+}
+
+func addTodo(todo string) error {
+    if len(todo) > 140 {
+        return errors.New("Todo is too long\n")
+    } else if len(todo) == 0 {
+        return errors.New("Todo is empty\n")
+    }
+
+    dbconn, err := getConnection()
+    if err != nil {
+        return err
+    }
+
+    _, err = dbconn.Exec(context.Background(), "INSERT INTO todos (todo) VALUES ($1)", todo)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
 
 func todoHandler(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
     case http.MethodGet:
         w.Header().Set("Content-Type", "application/json")
-        err := json.NewEncoder(w).Encode(todos)
+        todos, err := getTodos()
         if err != nil {
-            fmt.Printf("Error responding to get request: %s\n", err.Error())
+            fmt.Fprintf(os.Stderr, "Error reading todos from database: %s\n", err.Error())
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+            return
+        }
+
+        err = json.NewEncoder(w).Encode(todos)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Error responding to get request: %s\n", err.Error())
         }
     case http.MethodPost:
         err := r.ParseForm()
         if err != nil {
-            fmt.Printf("Error parsing post request form: %s\n", err.Error())
+            fmt.Fprintf(os.Stderr, "Error parsing post request form: %s\n", err.Error())
             return
         }
 
         todo := r.PostForm.Get("todo")
-        if len(todo) == 0 {
-            fmt.Printf("No value for todo\n")
-            http.Error(w, "No value for todo", http.StatusBadRequest)
-            return
-        } else if len(todo) > 140 {
-            http.Error(w, "Too long todo", http.StatusBadRequest)
+        err = addTodo(todo)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Error adding todo: %s\n", err.Error())
+            http.Error(w, err.Error(), http.StatusBadRequest)
             return
         }
 
-        todos = append(todos, todo)
         http.Redirect(w, r, r.Header.Get("Referer"), 302)
     default:
         http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
@@ -56,6 +131,26 @@ func main() {
             port = envport
         }
     }
+
+    fmt.Printf("Initializing postgres connection\n")
+    conn, err := getConnection()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Unable to connect to database: %s\n", err.Error())
+        os.Exit(1)
+    }
+    defer conn.Close(context.Background())
+    fmt.Printf("Succesfully opened connection to database\n")
+
+    cmd := `CREATE TABLE IF NOT EXISTS todos (
+                id SERIAL PRIMARY KEY,
+                todo varchar(140) NOT NULL
+            )`
+    _, err = conn.Exec(context.Background(), cmd)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Couldn't create postgresql table: %s\n", err.Error())
+        os.Exit(1)
+    }
+
     addr := fmt.Sprintf(":%d", port)
     fmt.Printf("Server started in port %d\n", port)
 
